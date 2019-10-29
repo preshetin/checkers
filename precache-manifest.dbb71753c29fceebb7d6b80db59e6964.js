9c3cce3ac267f1650e941efcbfa610bfde9a9054
Rik Brutsaert <dagnabit@gmail.com>
2019-10-28 17:26:10 -0700

    Updates

---
+++ precache-manifest.dbb71753c29fceebb7d6b80db59e6964.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "267de28fe70a3ced3ac6eceac71ed176",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aac3582f10951ed2efb4",
+    "url": "/static/js/2.267fb862.chunk.js"
+  },
+  {
+    "revision": "a5ea9f025e6a43bdbbab",
+    "url": "/static/js/main.966258b2.chunk.js"
+  },
+  {
+    "revision": "ec58fe622b4b072ce098",
+    "url": "/static/js/runtime-main.ed5a2601.js"
+  }
+]);
